f8d54ba9c2f79f0d8b14f5ce0cca2a1207fbbbf0
lahirutech <rmllbrathnayaka@gmail.com>
2021-01-25 14:46:50 +0530

    missing

---
+++ Dulya.github.io/js/Structure.js
@@ -0,0 +1,109 @@
+
+/**
+* Author: Dulya Murage
+*/
+function MetaphorDescriptor(mtype, dimension, dimensions, connectedSegments, position=new THREE.Vector3(0,0,0), level=0) {
+  this.metaphorType = mtype; // main (0) or sub >=(1)
+  this.mainDimension = dimension;
+  this.dimensions = dimensions;
+  this.connectedSegments = connectedSegments;
+  this.position = position;
+  this.level = level;
+ 
+}
+
+/*
+  Main Node (Parent)
+*/
+function Node() {
+  this.id = 0;
+  this.metaphor=null;
+  this.connectors=[];
+
+  this.metaphorDescriptor = null;
+}
+
+Node.prototype.getActiveDimensions = function() {  //Prototypes are the mechanism by which JavaScript objects inherit features from one another.
+  var result = [];
+  if(this.metaphor) {
+    var dimensions = Object.keys(this.metaphor.userData.segments);  //The Object.keys() method returns an array of a given object's own enumerable property names, iterated in the same order that a normal loop would.
+    for(var i=0; i<dimensions.length; i++){
+      var segment = this.metaphor.userData.segments[dimensions[i]];
+      if(segment.userData.isActive){
+        result.push(segment.userData.dimension);
+      }
+    }
+  }
+  return result;
+}
+Node.prototype.getLockedDimensions = function() {
+  var result = [];
+  if(this.metaphor) {
+    var dimensions = Object.keys(this.metaphor.userData.segments);
+    for(var i=0; i<dimensions.length; i++){
+      var segment = this.metaphor.userData.segments[dimensions[i]];
+      if(segment.userData.isLocked){
+        result.push(segment.userData.dimension);
+      }
+    }
+  }
+  return result;
+}
+
+Node.prototype.updateConnector = function(scene) {
+  this.connectors.forEach(connector => {
+    scene.remove(connector);
+  });
+  this.connectors = [];
+  this.metaphorDescriptor.connectedSegments.forEach(segment => {
+    var connector = createConnector(segment, this.metaphor);
+    scene.add(connector);
+    this.connectors.push(connector);
+  });
+  
+}
+
+Node.prototype.getConnectedNode = function() {
+  if (this.metaphorDescriptor.connectedSegments.length > 0 ) {
+    return this.metaphorDescriptor.connectedSegments[0].getnode();
+  }
+  return null;
+}
+
+Node.prototype.add = function(scene) {
+  if(this.metaphor) {
+    scene.add(this.metaphor);
+    this.updateConnector(scene);
+  }
+}
+Node.prototype.remove = function(scene) {
+  if (this.metaphor) {
+    scene.remove(this.metaphor);
+  }
+
+  this.connectors.forEach(connector => {
+    scene.remove(connector);
+  });
+  this.connectors = [];
+
+}
+
+function CreateNewNode(metaphorDescriptor){
+   var node = new Node();
+
+   node.metaphorDescriptor = metaphorDescriptor;
+   
+  if(metaphorDescriptor.metaphorType == 0){ // metaphorType==0 in main metaphor--> no middle button
+    node.metaphor = createCarousel(node, false);
+  }else{
+    node.metaphor = createCarousel(node,true);
+  }
+   // check metaphor types and create appropriate metaphors
+   
+
+   node.metaphor.position.x = metaphorDescriptor.position.x;
+   node.metaphor.position.y = metaphorDescriptor.position.y;
+   node.metaphor.position.z = metaphorDescriptor.position.z;
+
+   return node;
+}
---
+++ Metaphor-placement-on-districts/DistrictDataExplorer/ClickCarousel.js
@@ -0,0 +1,101 @@
+// var isCarouselSelected = false;
+// 		var isCasaroul = true;
+
+
+// var mouseDragCount = 0;
+// //window.addEventListener('resize', onWindowResize, false);
+// //window.addEventListener('mouseup', onMouseUp, false);
+// //window.addEventListener('mousedown', onMouseDown, false);
+// window.onmousemove = onMouseMove; //addEventListener('onmousemove', onMouseMove, false);
+
+// // var isConeDragging = false;
+// // 		function onMouseMove(event) {
+// // 			//console.log("Moving");
+// // 			dragCone(event);
+// // 		}
+
+
+// function onMouseDown(event) {
+//     event.preventDefault();
+//    // startDragCone(event);
+
+
+//     if (mouseDragCount < 5) {
+
+//         var raycaster = new THREE.Raycaster();
+//         var mouse = new THREE.Vector2();
+//         var intersects;
+//         mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+//         mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+//         raycaster.setFromCamera(mouse, camera);
+
+//         if (isCasaroul) {
+//             var prevNumLevels = dataExplorer.levels.length;
+//             // get all segments and buttons in the scene
+//             var i = 0;
+//             var children_segments = [];
+//             var allNodes = dataExplorer.getAllNodes();
+
+//             for (i = 0; i < allNodes.length; i++) {
+//                 children_segments = children_segments.concat(allNodes[i].metaphor.children);
+//             }
+
+
+//             var intersects = raycaster.intersectObjects(children_segments, true);
+//             //console.log(allActiveNodes);											
+//             var time = new Date() * 0.001;
+//             for (i = 0; i < intersects.length; i++) {
+//                 if (intersects[i].object.geometry.type == 'ExtrudeGeometry' || intersects[i].object.geometry.type == "SphereGeometry") {
+//                     isSegmentClicked = true;
+
+//                     // When the user clicks on the button, open the modal
+
+//                     clickedSlice = intersects[i].object;
+
+//                     clickedSlice.select(dataExplorer);
+//                     /*
+//                     if(clickedSlice.getType() == "segment"){
+//                         //clickedDimension = clickedSlice.getDimension();
+//                         clickedSlice.select(dataExplorer);
+//                     }else if(clickedSlice.getType() == "middle_button"){
+//                         clickedSlice.select(dataExplorer);
+//                     }*/
+
+
+//                     break;
+//                 }
+//             }
+//             /*
+//             var curNumLevels = dataExplorer.levels.length;
+//             if(curNumLevels>prevNumLevels && curNumLevels>3){
+//                 var lev = curNumLevels-1;
+//                 var zooms = [0,0,0,1.36*iniZoom,1.5*iniZoom, 1.7*iniZoom];
+
+//                 var curZoom = controls.target.distanceTo( controls.object.position );
+                
+//                 if (curZoom < zooms[lev]){
+//                     controls.zoomOut(zooms[lev]);
+//                     controls.update();
+//                 }
+                
+//             }*/
+//         }
+//     }
+//     mouseDragCount = 0;
+// }
+
+// function checkMouseIntersect(obj, mouseEvent){
+//     var raycaster = new THREE.Raycaster();
+//     var mouse = new THREE.Vector2();
+//     var intersects;
+//     mouse.x = (mouseEvent.clientX / window.innerWidth) * 2 - 1;
+//     mouse.y = -(mouseEvent.clientY / window.innerHeight) * 2 + 1;
+//     raycaster.setFromCamera(mouse, camera);
+
+//     var intersects = raycaster.intersectObjects([obj], true);
+//     console.log("LEVEL2",intersects)
+//     if(intersects.length > 0){
+//         return intersects[0];
+//     }
+//     return false;
+// }
